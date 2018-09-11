9b180811fd43e814ff91fbebe733db7ac44072d3
hrbrmstr <bob@rud.is>
2018-09-11 15:34:56 -0400

    R package repo initialization complete

---
+++ R/arabia-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/arabia>
+#' - BugReports: <https://gitlab.com/hrbrmstr/arabia/issues>
+#' 
+#' @md
+#' @name arabia
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
